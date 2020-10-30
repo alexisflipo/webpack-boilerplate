1005b6e0717e7dac2554ca7e071c7f7bbcfffadf
Alexis Flipo <alexis.flipo1@gmail.com>
2020-10-30 10:56:49 +0100

    function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello from src/index.js!");
+function abracadabra() {
+
+  let prenom = prompt('Quel est votre prénom ?');
+  let nom = prompt('Quel est votre nom ?');
+  let age = prompt('Quel est votre age ?');
+
+  alert("Sapristi ! On ne m'avait pas prévenu que c'était vous, "
+    + prenom
+    + " ! Euh... Je veux dire... Monsieur le grand magicien "
+    + nom
+    + " ! Cela fait déjà "
+    + age
+    + " ans que vous faites rayonner notre contrée !");
+}
+
+abracadabra();
