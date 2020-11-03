59b1fb217ca8c6cb49df6ee81f4ebc8443857131
Alexis Flipo <alexis.flipo1@gmail.com>
2020-11-03 11:12:12 +0100

    Conditions

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
-function abracadabra() {
+// function abracadabra() {
 
-  let prenom = prompt('Quel est votre prénom ?');
-  let nom = prompt('Quel est votre nom ?');
-  let age = prompt('Quel est votre age ?');
+//   let prenom = prompt('Quel est votre prénom ?');
+//   let nom = prompt('Quel est votre nom ?');
+//   let age = prompt('Quel est votre age ?');
 
-  alert("Sapristi ! On ne m'avait pas prévenu que c'était vous, "
-    + prenom
-    + " ! Euh... Je veux dire... Monsieur le grand magicien "
-    + nom
-    + " ! Cela fait déjà "
-    + age
-    + " ans que vous faites rayonner notre contrée !");
+//   alert("Sapristi ! On ne m'avait pas prévenu que c'était vous, "
+//     + prenom
+//     + " ! Euh... Je veux dire... Monsieur le grand magicien "
+//     + nom
+//     + " ! Cela fait déjà "
+//     + age
+//     + " ans que vous faites rayonner notre contrée !");
+// }
+
+// function calculerIMC(poids, taille) {
+//   let resultat = (poids /Math.pow((taille / 100), 2));
+//   return resultat;
+// }
+
+// let poids = prompt("Quel est votre poids ? (en kg)")
+// let taille = prompt("Quel est votre taille ? (en cm)")
+
+// alert(calculerIMC(poids, taille));
+
+let age = prompt("Quel est votre age ?")
+if (age < 18) {
+  alert("Vous n'êtes pas majeur");
+} else if (age < 21) {
+  alert("Vous êtes majeur en France");
+} else {
+  alert("Vous êtes majeur partout");
 }
-
-function calculerIMC(poids, taille) {
-  let resultat = (poids /Math.pow((taille / 100), 2));
-  return resultat;
-}
-
-let poids = prompt("Quel est votre poids ? (en kg)")
-let taille = prompt("Quel est votre taille ? (en cm)")
-
-alert(calculerIMC(poids, taille));
-
